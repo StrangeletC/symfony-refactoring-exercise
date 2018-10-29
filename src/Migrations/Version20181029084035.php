323916c82a88d1c0a92741ac94b20d53015f55ff
Alexander Abrosimov <StrangeletC@gmail.com>
2018-10-29 13:23:20 +0400

    Обновил миграцию в соответствии с моделью

---
+++ src/Migrations/Version20181029084035.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181029084035 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE task (id INTEGER NOT NULL, text VARCHAR(255) NOT NULL, completed BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('DROP TABLE todos');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE todos (id INTEGER DEFAULT NULL, text VARCHAR(255) DEFAULT NULL COLLATE BINARY, completed INTEGER DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('DROP TABLE task');
+    }
+}
