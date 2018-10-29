964540d72252bd1dc714c790c8a4f657370f3647
Alexander Abrosimov <StrangeletC@gmail.com>
2018-10-29 15:23:07 +0400

    Сырые SQL запросы заменил на Doctrine ORM. Разделил отображение завешренных и незавершенных задач в разные методы.

--- src/Controller/TodosController.php
+++ src/Controller/TodosController.php
@@ -2,32 +2,77 @@
 
 namespace App\Controller;
 
-use Doctrine\DBAL\Connection;
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TodosController extends AbstractController
 {
-    public function showTodos(Connection $connection)
+    /**
+     * Отображает все незавершенные задачи.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showTodos()
     {
-        if (isset($_GET['all']) && $_GET['all'] == '1') {
-            $todos = $connection->fetchAll('SELECT t.* FROM todos t');
-        } else {
-            $todos = $connection->fetchAll('SELECT t.* FROM todos t WHERE completed = 0');
-        }
+        $todos = $this->getDoctrine()->getRepository(Task::class)->findBy(['completed' => false]);
 
         return $this->render('showTodos.html.twig', ['todos' => $todos]);
     }
 
-    public function completeTodo(Connection $connection)
+    /**
+     * Отображает все завершенные задачи.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showCompletedTodos()
     {
-        $connection->executeQuery('UPDATE todos SET completed = 1 WHERE id = ' . $_GET['id']);
+        $todos = $this->getDoctrine()->getRepository(Task::class)->findBy(['completed' => true]);
+
+        return $this->render('showTodos.html.twig', ['todos' => $todos]);
+    }
+
+    /**
+     * Помечает задачу как завершенную.
+     *
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function completeTodo($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $todo = $entityManager->getRepository(Task::class)->find($id);
+
+        if (!$todo) {
+            throw $this->createNotFoundException(
+                'Задача не найдена'
+            );
+        }
+
+        $todo->setCompleted(true);
+        $entityManager->flush();
 
         return $this->redirect('/');
     }
 
-    public function uncompleteTodo(Connection $connection)
+    /**
+     * Помечает задачу как незавершенную.
+     *
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function uncompleteTodo($id)
     {
-        $connection->executeQuery('UPDATE todos SET completed = 0 WHERE id = ' . $_GET['id']);
+        $entityManager = $this->getDoctrine()->getManager();
+        $todo = $entityManager->getRepository(Task::class)->find($id);
+
+        if (!$todo) {
+            throw $this->createNotFoundException(
+                'Задача не найдена'
+            );
+        }
+
+        $todo->setCompleted(false);
+        $entityManager->flush();
 
         return $this->redirect('/');
     }
